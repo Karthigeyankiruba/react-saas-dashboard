11268f9a28f407308f91d0bd10862765ad2b7167
Karthigeyankiruba <karthigeyan0901@gmail.com>
2025-09-21 17:35:15 +0530

    feat: configure Vite build and server settings for improved development experience

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,21 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+  },
 })
